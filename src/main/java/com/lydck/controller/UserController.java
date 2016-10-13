20826fdd2109f731f620ca4ccf8aa5b70243a81f
Lydck <1468390049@qq.com>
2016-10-14 02:01:13 +0800

    spring-mvc 添加logback和修正

---
+++ src/main/java/com/lydck/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.lydck.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	//返回一个ModelAndView对象
+	@RequestMapping("/register")
+	public String register() {
+		System.out.println("register");
+		return "user/register";
+	}
+}
